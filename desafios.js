fad7d5fa9492ee9d7afd8bd270088449103ddad3
Geysabarros <128394647+Geysabarros@users.noreply.github.com>
2023-11-08 15:29:18 -0300

    curso concluido

---
+++ desafios.js
@@ -0,0 +1,130 @@
+// alert("Boas vindas ao nosso site");
+// let nome = "Lua";
+// let idade = 25;
+// let numeroDeVendas = 50;
+// saldoDisponivel = 1000;
+
+// alert("Erro! Preencha todos os campos");
+// let mensagemDeErro = "Erro! Preencha todos os campos.";
+
+// alert(mensagemDeErro);
+// let nome = prompt("Digite seu nome");
+
+// let idade = prompt("Digite sua idade");
+// if(idade >= 18) {
+//     alert("Pode tira a habilitação");
+// }
+
+// diaDaSemana = prompt('Qual é o dia da semana?');
+// if(diaDaSemana == 'Sábado') {
+//     alert('Bom fim de semana!');
+// }else if(diaDaSemana == 'Domingo') {
+//     alert('Bom fim de semana!');
+// }else {
+//     alert('Boa semama!');
+// }
+
+// numero = prompt("Digite um número");
+// if(numero < 0) {
+//     alert("Número negativo");
+// } else {
+//     alert("Número positivo");
+// }
+
+// let pontuacao = 444
+// if(pontuacao >= 100) {
+//     console.log("Parábens, você venceu");
+//  }else{
+//     console.log("Tente novamente para ganhar");
+//  }
+
+// let saldo = 550;
+// alert(`Você possui um saldo de ${saldo}`);
+
+// let nome = prompt("Insira seu nome");
+// alert(`Seja bem vindo, ${nome}!`);
+
+// let contador = 10;
+// while (contador >= 0) {
+//     console.log(contador);
+//     contador--;
+// }
+
+// let numero = prompt('digite um numero para a contagem regressiva:')
+
+// while(numero >= 0) {
+//     console.log(numero);
+//     numero--;
+// }
+
+// let numeroMaximo = prompt('Digite um numero para a contagem');
+// let contator = 0
+// while(contator <= numeroMaximo){
+//     console.log(contator);
+//     contator++;
+// }
+
+// console.log('Boas vindas!');
+
+// let nome = "geysa";
+// console.log (`Olá, ${nome}!`);
+
+// let nome = 'geysa';
+// alert(`Olá, ${nome}!`);
+
+// let linguagemPreferida = prompt ('Qual a linguagem de programação que você mais gosta?');
+// console.log (linguagemPreferida);
+
+// let valor1 = 5;
+// let valor2 = 8;
+// let resultado = valor1 + valor2;
+
+// console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}.`)
+
+
+// let valor1 = 28;
+// let valor2 = 18;
+// let resultado = valor1 - valor2;
+// console.log(`A diferença entre ${valor1} e ${valor2} é igual a ${resultado}.`);
+
+// let idade = prompt('Qual é a sua idade?');
+// if(idade >= 18) {
+//     console.log("Você é maior de idade");
+// } else {
+//     console.log("Você é menor de idade.");
+// }
+
+// let numero = prompt('Digite um número');
+// if(numero < 0){
+//     console.log('O número é negativo');
+// } else if(numero > 0) {
+//         console.log('O número é positivo');
+// } else {
+//     console.log('O número é zero');
+// }
+
+// let numero = 1;
+// while (numero <= 10) {
+//     console.log(numero);
+//     numero++;
+// }
+
+//10
+// let nota = 5
+// if (nota >= 7) {
+//     console.log('aprovado');
+// } else {
+//     console.log('reprovado');
+// }
+
+//11
+// let numero = Math.random();
+// console.log(numero);
+
+//12
+// let numeroAleatorio = parseInt(Math.random() *10) +1;
+// console.log(numeroAleatorio);
+
+//13
+let numeroAleatorio = parseInt(Math.random() *1000) +1;
+console.log(numeroAleatorio);
